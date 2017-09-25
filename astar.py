494eea2f8240ddfd09378895f7511b8354f4fbfd
Vignesh M.P <vignesh9025@yahoo.com>
2017-09-25 02:57:40 -0600

    Recursive

---
+++ astar.py
@@ -0,0 +1,99 @@
+# Recursive Best First Search (Figure 3.26, Russell and Norvig)
+#  Recursive Iterative Deepening form of A*, where depth is replaced by f(n)
+import time
+class Node:
+    def __init__(self, state, f=0, g=0 ,h=0):
+        self.state = state
+        self.f = f
+        self.g = g
+        self.h = h
+    def __repr__(self):
+        return "Node(" + repr(self.state) + ", f=" + repr(self.f) + \
+               ", g=" + repr(self.g) + ", h=" + repr(self.h) + ")"
+
+def aStarSearch(startState, actionsF, takeActionF, goalTestF, hF):
+    h = hF(startState)
+    startNode = Node(state=startState, f=0+h, g=0, h=h)
+    return aStarSearchHelper(startNode, actionsF, takeActionF, goalTestF, hF, float('inf'))
+
+# Global variable declaration
+
+
+
+def aStarSearchHelper(parentNode, actionsF, takeActionF, goalTestF, hF, fmax):
+	#print(parentNode)
+	if goalTestF(parentNode.state):
+		return ([parentNode.state], parentNode.g)
+	## Construct list of children nodes with f, g, and h values
+	actions = actionsF(parentNode.state)
+	if not actions:
+	    return ("failure", float('inf'))
+	children = []
+	for action in actions:
+	    (childState,stepCost) = takeActionF(parentNode.state, action)
+	    h = hF(childState)
+	    g = parentNode.g + stepCost
+	    f = max(h+g, parentNode.f)
+	    childNode = Node(state=childState, f=f, g=g, h=h)
+	    children.append(childNode)
+	nf = False
+	while True:
+	    # find best child
+	    children.sort(key = lambda n: n.f) # sort by f value
+	    # If f values of all children become infinity, then break.
+	    flag = 0
+	    for c in children:
+	        if c.f != float('inf'):
+	            flag = 1
+	    if flag == 0:
+	        nf = True
+	        break
+
+	    bestChild = children[0]
+	    if bestChild.f > fmax:
+	        return ("failure",bestChild.f)
+	    # next lowest f value
+	    alternativef = children[1].f if len(children) > 1 else float('inf')
+	    # expand best child, reassign its f value to be returned value
+	    result,bestChild.f = aStarSearchHelper(bestChild, actionsF, takeActionF, goalTestF, hF, min(fmax,alternativef))
+	    if result == "Not found":
+	        nf = True
+	    if result is not "failure" and result is not "Not found":               
+	        result.insert(0,parentNode.state)     
+	        return (result, bestChild.f)
+	if nf == True:
+		return ("Not found", bestChild.f)          
+						                           
+if __name__ == "__main__":                          
+                                                 
+    successors = {'a': ['b','c'],                 
+                  'b': ['d','e'],                       
+                  'c': ['f'],                     
+                  'd': ['g', 'h'],                
+                  'f': ['i','j']} 
+
+    successors2 = {'a': ['b', 'c']}
+
+    successors3 = {'a': ['b','c'],                 
+                  'b': ['d','e'],                       
+                  'c': ['f', 'a'],                     
+                  'd': ['g', 'h'],                
+                  'f': ['i','j']} 
+
+    def actionsF(s):                               
+        try:                                      
+            ## step cost of each action is 1
+            return [(succ,1) for succ in successors[s]]
+        except KeyError:
+            return []
+    def takeActionF(s,a):
+        return a
+    def goalTestF(s):
+        return s == goal
+    def h1(s):
+        return 0
+    start = 'a'
+    goal = 'l'
+    result = aStarSearch(start,actionsF,takeActionF,goalTestF,h1)
+    #print('Path from a to g is', result[0], 'for a cost of', result[1])
+    print(result)
