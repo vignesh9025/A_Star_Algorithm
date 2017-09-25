ab211f77a02aaf0ba75e32cfbed7ab230ddd64eb
Vignesh M.P <vignesh9025@yahoo.com>
2017-09-25 14:55:23 -0600

    Function to find Effective Branching Factor of a tree.

---
+++ ebf.py
@@ -0,0 +1,26 @@
+# 1 + b + b^2 + ... + b^d
+
+def ebf(nNodes, depth, precision = 0.01):
+	left = 1
+	right = nNodes
+	if nNodes == 0:
+		return 0
+	b = 1
+	while(True):
+		if abs(right - left) <= precision:
+			break
+		#print('left', left)
+		#print('right', right)
+		b = (left + right)/2
+		#print('b', b)
+		n = (pow(b, (depth+1)) - 1)/(b - 1)
+		if nNodes < n:
+			right = b - precision
+		elif nNodes > n:
+			left = b + precision
+		else:
+			return b
+	return b
+
+print(ebf(100, 12))
+
